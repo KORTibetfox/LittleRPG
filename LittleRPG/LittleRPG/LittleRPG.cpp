12bff14a70c5160b2db2e17b1b5304fc6acd9496
pysunn14 <161586699+pysunn14@users.noreply.github.com>
2024-04-09 01:04:01 +0900

    Add files via upload

---
+++ LittleRPG/LittleRPG/LittleRPG.cpp
@@ -0,0 +1,40 @@
+﻿// LittleRPG.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+#include <SFML/Graphics.hpp>
+
+using namespace std;
+class Game
+{
+    public:
+
+
+    private:
+        int a;
+};
+
+int main()
+{
+    cout << "console output " << endl;
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
+}
